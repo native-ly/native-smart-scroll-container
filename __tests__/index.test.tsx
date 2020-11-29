00069accce0f3c7788c04d644f0308743659fe99
Jakub Biesiada <biesiadajakub@icloud.com>
2020-11-29 21:44:57 +0100

    test: add failing tests

--- __tests__/index.test.tsx
+++ __tests__/index.test.tsx
@@ -5,19 +5,49 @@ import { render } from '@testing-library/react-native'
 import SmartScrollContainer from '../src'
 
 describe('SmartScrollContainer', () => {
-  it('should render container with disabled scroll', () => {})
+  // it('should render container with disabled scroll', () => {
 
-  it('should render container with enabled scroll', () => {
+  // })
+
+  it('should set scroll state automatically', () => {
     const { getByTestId } = render(
       <SmartScrollContainer testID="scroll-container" style={{ height: 500 }}>
         <View style={{ height: 1600 }} />
       </SmartScrollContainer>
     )
 
+    expect(true).toBe(false)
+
     // expect(getByTestId('scroll-container'))
   })
 
-  it('should render container with force enabled scroll', () => {})
+  // TODO force disabled
+  it('should render container with force enabled scroll', () => {
+    const { getByTestId } = render(
+      <SmartScrollContainer
+        testID="scroll-container"
+        scrollEnabled={true}
+        style={{ height: 500 }}
+      >
+        <View style={{ height: 1600 }} />
+      </SmartScrollContainer>
+    )
 
-  it('should render container with enabled horizontal scroll', () => {})
+    expect(true).toBe(false)
+  })
+
+  it('should render container with enabled horizontal scroll', () => {
+    const { getByTestId } = render(
+      <SmartScrollContainer
+        testID="scroll-container"
+        scrollEnabled={true}
+        horizontal
+        style={{ height: 500 }}
+      >
+        <View style={{ height: 1600 }} />
+      </SmartScrollContainer>
+    )
+
+    expect(true).toBe(false)
+  })
 })
