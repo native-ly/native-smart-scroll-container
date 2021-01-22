ae12b2f4537cc51f451ef60c553a0f95230b8128
Jakub Biesiada <biesiadajakub@icloud.com>
2021-01-22 22:33:59 +0100

    test: update

--- __tests__/index.test.tsx
+++ __tests__/index.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { View } from 'react-native'
-import { render, fireEvent } from '@testing-library/react-native'
+import { render } from '@testing-library/react-native'
 
 import SmartScrollContainer from '../src'
 
